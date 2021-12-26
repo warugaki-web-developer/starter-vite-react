b2e057eb113c823ae4bdbe4e3553957631bbf7b8
warugaki <warugaki.web.developer@gmail.com>
2021-12-26 14:54:31 +0900

    storybookでもemotionが機能するように（webpack → babel）

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,24 +2,8 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
-  webpackFinal: (config) => {
-    config.module.rules.push({
-      test: /\.(js|jsx)$/,
-      use: [
-        {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-react',
-                { runtime: 'automatic', importSource: '@emotion/react' },
-              ],
-            ],
-            plugins: ['@emotion/babel-plugin'],
-          },
-        },
-      ],
-    });
-    return config;
-  },
+  babel: async (options) => ({
+    ...options,
+    presets: [...options.presets, '@emotion/babel-preset-css-prop'],
+  }),
 };
