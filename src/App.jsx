742f4f6f9efb68ab75666554a819b5a2e3e1b609
warugaki <warugaki.web.developer@gmail.com>
2021-12-27 21:17:20 +0900

    app.css をemotionに（css-in-js）に移行

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
-import { Global } from '@emotion/react';
+import { Global, css } from '@emotion/react';
 import { useState } from 'react';
 
 import logo from './logo.svg';
 import globalStyle from './styles/global';
 
-import './App.css';
+const style = css`
+  & {
+    text-align: center;
+  }
+
+  .app-logo {
+    height: 40vmin;
+    pointer-events: none;
+  }
+
+  @media (prefers-reduced-motion: no-preference) {
+    .app-logo {
+      animation: app-logo-spin infinite 20s linear;
+    }
+  }
+
+  .app-header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    font-size: calc(10px + 2vmin);
+    color: white;
+    background-color: #282c34;
+  }
+
+  .app-link {
+    color: #61dafb;
+  }
+
+  @keyframes app-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  button {
+    font-size: calc(10px + 2vmin);
+  }
+`;
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -12,7 +56,7 @@ const App = () => {
   return (
     <>
       <Global styles={globalStyle} />
-      <div className="app">
+      <div className="app" css={style}>
         <header className="app-header">
           <img src={logo} className="app-logo" alt="logo" />
           <p>Hello Vite + React!</p>
