55b3033a0caf5144b430839148067f9df55e6a16
warugaki <warugaki.web.developer@gmail.com>
2021-12-27 21:04:34 +0900

    add globalstyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { Global } from '@emotion/react';
 import { useState } from 'react';
 
 import logo from './logo.svg';
+import globalStyle from './styles/global';
 
 import './App.css';
 
@@ -8,39 +10,42 @@ const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="app">
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount(count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="app-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="app-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <Global styles={globalStyle} />
+      <div className="app">
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+          <p>Hello Vite + React!</p>
+          <p>
+            <button type="button" onClick={() => setCount(count + 1)}>
+              count is: {count}
+            </button>
+          </p>
+          <p>
+            Edit <code>App.jsx</code> and save to test HMR updates.
+          </p>
+          <p>
+            <a
+              className="app-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            {' | '}
+            <a
+              className="app-link"
+              href="https://vitejs.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Vite Docs
+            </a>
+          </p>
+        </header>
+      </div>
+    </>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'sanitize.css';
 
 import App from './App';
-
-import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
