6f7a58143d1f4048fed37884b02da9ccdae71e75
warugaki <warugaki.web.developer@gmail.com>
2021-12-28 09:24:03 +0900

    エントリーファイルをsrc配下で管理する

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './src',
+  build: {
+    outDir: '../dist',
+  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
